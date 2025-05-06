c8f0b14003c8b392beec08ac37f8b87bf3abc06d
Temiii857 <tps5749@psu.edu>
2025-05-05 23:56:34 -0400

    cytoscape network

--- sbsv10_scraper.py
+++ sbsv10_scraper.py
@@ -12,16 +12,13 @@ with open("vol-10.txt", "r", encoding="utf-8") as file:
 doc = nlp(text)
 
 
-adjectives = [token.text for token in doc if token.pos_ == "ADJ"]
-
-
 output_dir = "output"
 os.makedirs(output_dir, exist_ok=True)
 
 
 output_path = os.path.join(output_dir, "vol10_adjectives.tsv")
-with open("output/vol10_adjectives.tsv", "w", encoding="utf-8") as f:
-    f.write("Word\tPartOfSpeech\n")
+with open(output_path, "w", encoding="utf-8") as f:
+    f.write("Word\tVolume\tPartOfSpeech\n")
     for token in doc:
         if token.pos_ == "ADJ":
-            f.write(f"{token.text}\t{token.pos_}\n")
+            f.write(f"{token.text}\tVolume 10\t{token.pos_}\n")
